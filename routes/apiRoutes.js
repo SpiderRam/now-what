31246c868be3fe39d7471e038b79ff8238ea92ed
Reeshida <reeshida@hotmail.com>
2018-10-03 20:56:50 -0400

    added indeed scrape and fixed meetup data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,8 @@
 module.exports = function(app) {
 
   var request = require("request");
-  
+  var indeed = require('indeed-scraper');
+
   app.get("/udemy/:udemyQuery", function(req, res){
     var udemyQuery = req.params.udemyQuery;
     console.log(udemyQuery);
@@ -21,38 +22,102 @@ module.exports = function(app) {
     var youTubeQuery = req.params.youTubeQuery;
     console.log(youTubeQuery);
     request ({
-        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + youTubeQuery + "&type=video&key=" + process.env.YOU_TUBE_API,  
-    },function(err, raw, body){
-      res.json(body)
-      console.log(body);
-    });
-  });
-  
-
-  app.get("/meetup", function(req, res){
-
-    request ({
-     url: " https://api.meetup.com/find/upcoming_events?photo-host=public&page=20&sig_id=258526652&sig=" + process.env.MEET_UP_KEY,
+      url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=dogs=&type=video&key=" + process.env.YOU_TUBE_API,
+        
         
     },function(err, raw, body){
-      console.log(JSON.parse(body).events[0].name);
-      console.log(JSON.parse(body).events[0].link);
-      var cleanMeetup = []
-        for(var i=0; i<JSON.parse(body).events.length; i++){
-          var singleMeetup = {
-            title: JSON.parse(body).events[i].name,
-            link: JSON.parse(body).events[i].link,
-            picture: "https://pbs.twimg.com/profile_images/875701356849504256/x8t7RxeV_400x400.jpg"
+      var cleanYoutube = []
+       for(var i=0; i<JSON.parse(body).items.length; i++){
+         var singleYoutube = {
+           link: JSON.parse(body).items[i].id.videoId,
+           picture: JSON.parse(body).items[i].snippet.thumbnails.default.url,
+           title: JSON.parse(body).items[i].snippet.title
 
-          }
+         }
 
-          cleanMeetup.push(singleMeetup);
-
+         cleanYoutube.push(singleYoutube);
         }
+
+      console.log(JSON.parse(body).items[0].id.videoId);
+      console.log(JSON.parse(body).items[0].snippet.thumbnails.default.url);
+      console.log(JSON.parse(body).items[0].snippet.title);
+      // res.json(JSON.parse(body).items[0]);
+      res.json(cleanYoutube);
+      // console.log(body);
+    })
+  })
+
+  app.get("/meetup", function(req, res){
+   
+    // var url = "https://api.meetup.com/find/upcoming_events?photo-host=public&topic_category=tech&page=20&sig_id=258526652&radius=50&sig=" + process.env.MEET_UP_KEY;
+    // console.log(url);
+    request ({
+      url: "https://api.meetup.com/find/upcoming_events?&photo-host=public&topic_category=34&page=5&radius=50&key=" + process.env.MEET_UP_KEY
+
+    //  url: " https://api.meetup.com/find/upcoming_events?photo-host=public&topic_category=tech&page=20&sig_id=258526652&radius=50&sig=" + process.env.MEET_UP_KEY,
+      //  url: "https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&topic_category=tech&page=20&radius=50&key=" + process.env.MEET_UP_KEY,
+    },function(err, raw, body){
+      // console.log(JSON.parse(body))
+      // console.log(JSON.parse(body).events[0].name);
+      // console.log(JSON.parse(body).events[0].link);
+      var cleanMeetup = []
+       for(var i=0; i<JSON.parse(body).events.length; i++){
+         var singleMeetup = {
+           title: JSON.parse(body).events[i].name,
+           link: JSON.parse(body).events[i].link,
+           picture: "https://pbs.twimg.com/profile_images/875701356849504256/x8t7RxeV_400x400.jpg"
+
+         }
+
+         cleanMeetup.push(singleMeetup);
+
+       }
       
+
+
+
+
       res.json(cleanMeetup);
       // console.log(body);
-    });
-  });
-};
+    })
+  })
+  app.get("/indeed", function(req, res){
+    var jobsArr = [];
 
+    const queryOptions = {
+      query: 'Web Developer',
+      city: 'Richmond, VA',
+      radius: '50',
+      level: 'entry_level',
+      jobType: 'fulltime',
+      maxAge: '7',
+      sort: 'date',
+      limit: '20'
+    };
+    var jobsArr = [];
+    indeed.query(queryOptions).then(data => {
+      
+    
+        // // console.log(res); // An array of Job objects
+        // for (let i = 0; i < res.length; i++) {
+        //   jobsArr.push = res[i];
+          
+        // }
+      
+        // console.log(jobsArr);
+        // res.json(jobsArr);
+
+        res.json(data)
+    });
+
+
+  })
+}
+//         url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + youTubeQuery + "&type=video&key=" + process.env.YOU_TUBE_API,
+        
+//     },function(err, raw, body){
+//       res.json(body)
+//     });
+//   });
+
+// };
